6b0e3dc207ac3bccd87a3e22869c22048df79165
troist <troist@gmail.com>
2013-08-04 10:43:05 +0100

    Added copy/paste client server files. chat.py can crash server.py so progress?

--- chat.py
+++ chat.py
@@ -11,12 +11,27 @@
 from sysUtils import vPrint
 import sysUtils
 
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol
+from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
+
 ##########################################
 
+class Greeter(Protocol):
+    def sendMessage(self, msg):
+        self.transport.write("MESSAGE %s\n" % msg)
 
+def gotProtocol(p):
+    p.sendMessage("Hello")
+    reactor.callLater(1, p.sendMessage, "This is sent in a second")
+    reactor.callLater(2, p.transport.loseConnection)
 
 ##########################################
 
 if __name__ == '__main__':
     sysUtils.setFlags()
-    print 'Hello World!'
+    point = TCP4ClientEndpoint(reactor, "localhost", 1234)
+    d = connectProtocol(point, Greeter())
+    d.addCallback(gotProtocol)
+    reactor.run()
+    print "Chat client running"
--- server.py
+++ server.py
@@ -11,7 +11,35 @@
 from sysUtils import vPrint
 import sysUtils
 
+from twisted.internet import protocol, reactor
+from twisted.internet.protocol import Protocol
+
 ##########################################
+
+PORT = 1234
+
+##########################################
+
+class Echo(Protocol):
+
+    def __init__(self, factory):
+        self.factory = factory
+
+    def connectionMade(self):
+        self.factory.numProtocols = self.factory.numProtocols+1 
+        self.transport.write(
+            "Welcome! There are currently %d open connections.\n" %
+            (self.factory.numProtocols,))
+
+    def connectionLost(self, reason):
+        self.factory.numProtocols = self.factory.numProtocols-1
+
+    def dataReceived(self, data):
+        self.transport.write(data)
+
+class EchoFactory(protocol.Factory):
+    def buildProtocol(self, addr):
+        return Echo()
 
 
 
@@ -19,4 +47,8 @@ import sysUtils
 
 if __name__ == '__main__':
     sysUtils.setFlags()
-    print 'Hello World!'
+    
+    reactor.listenTCP(PORT, EchoFactory())
+    reactor.run()
+
+    print 'Server listening on port ' + str(PORT)
